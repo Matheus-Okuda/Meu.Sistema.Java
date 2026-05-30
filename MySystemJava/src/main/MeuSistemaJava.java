7874f267ce288c29f5670e9519c8e0c4f1ee6585
mathe <mathe@Yuuk>
2026-05-30 13:09:29 -0300

    v1.0 String + Int.

---
+++ MySystemJava/src/main/MeuSistemaJava.java
@@ -0,0 +1,21 @@
+package main;
+
+public class MeuSistemaJava {
+ 
+	public static void main(String[] args) {
+		
+		int moduloAtual = 7;		
+		int certificados = 3; 
+		
+		String Nome = "Ayum"; 
+		String Curso = "Java Full Stack"; 
+		
+		System.out.println("===================");
+		System.out.println("Meu Sistema Java");
+		System.out.println("==================");
+		System.out.println("Nome: " + Nome);
+		System.out.println("Curso: " + Curso);
+		System.out.println("Mudolo atual: " + moduloAtual);
+		System.out.println("Certificados: " + certificados);
+	}
+}
